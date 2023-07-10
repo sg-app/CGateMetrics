e63566c785156249686c95f642ff0a660cabc98b
Georg Schreiner <uid15076@contiwan.com>
2023-07-10 10:52:35 +0200

    init

---
+++ CGateMetrics/CGateMetricsData/CGateMetricsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGateMetricsData
+{
+    public class CGateMetricsDbContext : DbContext
+    {
+        public CGateMetricsDbContext(DbContextOptions<CGateMetricsDbContext> options) : base (options)
+        {
+            
+        }
+
+    }
+}
---
+++ CGateMetrics/CGateMetricsData/RegisterData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGateMetricsData
+{
+    public static class RegisterData
+    {
+        public static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CGateMetricsDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("default"));
+            });
+
+            return services;
+        }
+    }
+}
