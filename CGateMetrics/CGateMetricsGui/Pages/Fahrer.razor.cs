b93a59e1d7220dbbeffc81e74edce66647c05603
Kettner Stephan (uid14650) <Stephan.Kettner@continental-corporation.com>
2023-07-11 14:33:16 +0200

    feat: fahrer razor delete

--- CGateMetrics/CGateMetricsGui/Pages/Fahrer.razor.cs
+++ CGateMetrics/CGateMetricsGui/Pages/Fahrer.razor.cs
@@ -1,11 +1,15 @@
 ﻿using CGateMetricsData;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
+using Radzen;
+using Radzen.Blazor;
 
 namespace CGateMetricsGui.Pages
 {
     public partial class Fahrer
     {
+
+        private RadzenDataGrid<CGateMetricsData.Models.Fahrer> _fahrerGrid;
 
         [Inject] 
         public CGateMetricsDbContext _context { get; set; }
@@ -16,6 +20,33 @@ namespace CGateMetricsGui.Pages
         {
              _fahrer = await _context.Fahrer.ToListAsync();
         }
+
+
+        protected async Task EditButton(CGateMetricsData.Models.Fahrer item)
+        {
+
+        }
+
+
+        protected async Task DeleteButton(CGateMetricsData.Models.Fahrer item)
+        {
+            bool? deleteConfirm = await DialogService.Confirm("Sind Sie sicher?", "Fahrer löschen", new ConfirmOptions() { OkButtonText = "Ja", CancelButtonText = "Nein" });
+
+            if (deleteConfirm == true) 
+            { 
+                _fahrer.Remove(item);
+
+                var fahrer = await _context.Fahrer.FindAsync(item.AusweisId);
+                _context.Fahrer.Remove(fahrer);
+                await _context.SaveChangesAsync();
+                await _fahrerGrid.Reload(); 
+
+            }
+
+
+
+        }
+
     }
 }
 
