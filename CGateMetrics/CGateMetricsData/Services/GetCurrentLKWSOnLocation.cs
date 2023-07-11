d57c0d91778698851c7cdf7ee76a24781a3ad99c
Onur Satir <uidp3714@contiwan.com>
2023-07-11 09:59:56 +0200

    Fixed classses

--- CGateMetrics/CGateMetricsData/Services/GetCurrentLKWSOnLocation.cs
+++ CGateMetrics/CGateMetricsData/Services/GetCurrentLKWSOnLocation.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using CGateMetricsData.Interfaces;
 using CGateMetricsData.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CGateMetricsData.Services
 {
@@ -14,24 +16,14 @@ namespace CGateMetricsData.Services
 
         public async Task<List<Fahrzeug>> GetCurrentLKWSOnLocation(string location)
         {
+           var buchung =  _context.Buchungen.Where( i => i.Standort == location &&  i.UhrzeitOut == null).Include(f=> f.Fahrzeug);
+           
+         return  await buchung.Select(b => b.Fahrzeug).ToListAsync();
 
-            var currentLKWS =  _context.Buchungen.FirstOrDefault(f => f.Standort == location);
-
-            
-            var lkwsonlocation = new List<Fahrzeug>
-            {
-                currentLKWS.Fahrzeug
-        };
-
-            return lkwsonlocation;
-
-
-
-
-
+         
         }
-
 
 
     }
 }
+ 
