b1bdaafdbbdbc6e4ddbab543e5e7871f65bd886f
Georg Schreiner <uid15076@contiwan.com>
2023-07-12 11:47:51 +0200

    add diagramms2

---
+++ CGateMetrics/CGateMetricsGui/Pages/Auswertungen2Page.razor.cs
@@ -0,0 +1,51 @@
+﻿using CGateMetricsData;
+using CGateMetricsGui.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace CGateMetricsGui.Pages
+{
+    public partial class Auswertungen2Page
+    {
+        [Inject] public CGateMetricsDbContext Context { get; set; }
+
+        private List<CountLkwMonth> _countLkwMonthList = new();
+        private List<WeightDataItem> _weightDataItemList = new();
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _startTime = DateTime.Now.AddDays(-30).Date;
+            _endTime = DateTime.Now.Date;
+
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            _countLkwMonthList = await Context.Buchungen
+                .Where(f => f.UhrzeitIn > _startTime && f.UhrzeitIn < _endTime)
+                .GroupBy(f => f.UhrzeitIn.Date)
+                .Select(f =>
+                new CountLkwMonth
+                {
+                    Date = f.Key,
+                    Count = f.Count()
+                })
+                .ToListAsync();
+
+
+            _weightDataItemList = await Context.Buchungen
+                 .Where(f => f.UhrzeitIn > _startTime && f.UhrzeitIn < _endTime)
+                 .Select(f =>
+                     new WeightDataItem
+                     {
+                         Time = f.UhrzeitIn.TimeOfDay,
+                         Datetime = f.UhrzeitIn,
+                         Weight = f.GewichtIn
+                     })
+                 .ToListAsync();
+        }
+    }
+}
